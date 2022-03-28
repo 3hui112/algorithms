fad2829ba1595b80434022d27515a3515b634c77
3hui112 <shinetheworld112@gmail.com>
2022-03-28 18:01:01 +0900

    solve N.11021

--- stepByStep/for문/11021.py
+++ stepByStep/for문/11021.py
@@ -1,5 +1,11 @@
 T = int(input())
+A_list = []
+B_list = []
 
-for i in (1, T):
+for i in range(0, T):
     A, B = map(int, input().split())
-    print("Case #{0}:".format(i), A+B)
+    A_list.append(A)
+    B_list.append(B)
+    
+for i in range(0, T):
+    print("Case #{0}:".format(i+1), A_list[i]+B_list[i])
